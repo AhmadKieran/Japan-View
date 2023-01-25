d9bc27349510add7dddaf3128041ca4129f300d3
AhmdKieran <117082338+AhmadKieran@users.noreply.github.com>
2023-01-25 10:52:17 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
-
-
-new Swiper("#swiper-2", {
-    slidesPerView: 1,
+var swiper = new Swiper('#swiper-1', {
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    slidesOffsetBefore: 0,
+    speed: 800,
+    // spaceBetween: '8%',
+    slidesPerView: 1.5,
     centeredSlides: true,
-    spaceBetween: 5,
-    pagination: {
-        el: ("#swiper-2 .swiper-custom-pagination"),
-        clickable: true,
-        renderBullet: function (index, className) {
-        return `<div class=${className}>
-            <span class="number">${index + 1}</span>
-            <span class="line"></span>
-            </div>`;
-        }
+    paginationClickable: true,
+    effect: 'coverflow',
+    coverflow: {
+      rotate: 0,
+      stretch: 0,
+      depth: 50,
+      modifier: 3,
+      slideShadows : false
     },
-    lazyLoading: true,
+    autoplay: 
+    {
+      delay: 3000,
+    },
     loop: true,
     keyboard: {
         enabled: true,
     },
-    navigation: {
-        nextEl: "#nav-right",
-        prevEl: "#nav-left"
-    },
-    breakpoints: {
-        800: {
-            slidesPerView: 1.2
-        },
-        1400: {
-            slidesPerView: 3
-        }
-    }
-});
-
-
+    simulateTouch: true,
+    mousewheelControl: true
+  });
+  $(document).ready(function(){
+    
+   $('.swiper-wrapper .swiper-slide:not(.swiper-slide-active)').on('click', function(e){
+      e.preventDefault();
+      swiper.slideTo($(this).index());
+      return false;
+   });
+  });
